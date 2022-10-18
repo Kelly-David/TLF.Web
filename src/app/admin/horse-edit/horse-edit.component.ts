1be1e16c8121b3a93849146e40213c9c3eba3c6f
David <davidkelly.tlf@gmail.com>
2022-10-18 09:20:43 +0100

    More form work

--- src/app/admin/horse-edit/horse-edit.component.ts
+++ src/app/admin/horse-edit/horse-edit.component.ts
@@ -32,7 +32,16 @@ export class HorseEditComponent implements OnChanges {
       'FormHeight': ['', [Validators.required]],
       'FormProfile': ['', [Validators.required]],
       'FormOwner': ['', [Validators.required]],
-      'FormBreeder': ['', [Validators.required]]
+      'FormBreeder': ['', [Validators.required]],
+      'FormCheckAMHA': [],
+      'FormCheckAMHR': [],
+      'FormCheckASPC': [],
+      'FormCheckBMHS': [],
+      'FormCheckFilterReference': [],
+      'FormCheckFilterStallion': [],
+      'FormCheckFilterBreeding': [],
+      'FormCheckFilterMare': [],
+      'FormCheckFilterFoal': [],
     })
   }
 
@@ -53,7 +62,7 @@ export class HorseEditComponent implements OnChanges {
   private PatchForm(data: any) {
 
     this.form.patchValue({
-      FormId: data.id ? data.id: '',
+      FormId: data.id ? data.id : '',
       FormName: data.name ? data.name : '',
       FormSire: data.sire ? data.sire : '',
       FormDam: data.dam ? data.dam : '',
@@ -63,9 +72,64 @@ export class HorseEditComponent implements OnChanges {
       FormHeight: data.height ? data.height : '',
       FormProfile: data.profile ? data.profile : '',
       FormOwner: data.owner ? data.owner : '',
-      FormBreeder: data.breeder ? data.breeder : ''
+      FormBreeder: data.breeder ? data.breeder : '',
+      FormCheckAMHA: false,
+      FormCheckAMHR: false,
+      FormCheckASPC: false,
+      FormCheckBMHS: false,
+      FormCheckFilterReference: false,
+      FormCheckFilterStallion: false,
+      FormCheckFilterBreeding: false,
+      FormCheckFilterMare: false,
+      FormCheckFilterFoal: false
     });
 
-  }
+    data.registration.forEach((val: any) => {
 
+      switch (val) {
+        case "AMHA": {
+          this.form.patchValue({ FormCheckAMHA: true });
+          break;
+        }
+        case "AMHR": {
+          this.form.patchValue({ FormCheckAMHR: true });
+          break;
+        }
+        case "ASPC": {
+          this.form.patchValue({ FormCheckASPC: true });
+          break;
+        }
+        case "BMHS": {
+          this.form.patchValue({ FormCheckBMHS: true });
+          break;
+        }
+      }
+    });
+
+    data.filter.forEach((val: any) => {
+
+      switch (val) {
+        case "foal": {
+          this.form.patchValue({ FormCheckFilterFoal: true });
+          break;
+        }
+        case "stallion": {
+          this.form.patchValue({ FormCheckFilterStallion: true });
+          break;
+        }
+        case "mare": {
+          this.form.patchValue({ FormCheckFilterMare: true });
+          break;
+        }
+        case "breeding": {
+          this.form.patchValue({ FormCheckFilterBreeding: true });
+          break;
+        }
+        case "reference": {
+          this.form.patchValue({ FormCheckFilterReference: true });
+          break;
+        }
+      }
+    });
+  }
 }
